b9a7b1e2de290a2c782380534b60aafb7326632d
Meredith Spiers <meredithjspiers@gmail.com>
2021-10-09 18:00:48 -0500

    Added FSCalendar pod and Calendar functionality

--- CNSAPP/ViewController.swift
+++ CNSAPP/ViewController.swift
@@ -5,15 +5,31 @@
 //  Created by Robert Colman Loch on 10/5/21.
 //
 
+import FSCalendar
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FSCalendarDelegate{
+    
+    var calendar = FSCalendar()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        calendar.delegate = self
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        calendar.frame = CGRect(x: 0,
+                                y: 75,
+                                width: view.frame.size.width,
+                                height: view.frame.size.width)
+        view.addSubview(calendar)
+    }
 
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition){
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE, MMM. dd"
+        let string = formatter.string(from: date)
+        print("\(string) Announcements:")
+    }
 }
-
